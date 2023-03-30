8c426fe7aeb360e54fa35766b4a5a232f236d1d3
grufarik <austinegomilo7@gmail.com>
2023-03-30 15:19:27 -0700

    post

---
+++ post.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * post increment - value changes after executionof code
+ * i++: is used for post increment
+ */
+
+int main()
+{
+	int i=2;
+	i++; //the value of I will be incresed by one
+
+	printf("%d\n", i);
+}
