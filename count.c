52316376d5335bde7c7dfbbed3429bf1a9f6dfb6
grufarik <austinegomilo7@gmail.com>
2023-03-30 14:15:52 -0700

    count

---
+++ count.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * count numbers 1 to 10 followed by a new line.
+ * Return: Always 0 when successful
+ */
+int main()
+{
+	int i;
+	
+	for (i = 1; i <= 10; i++)
+	{
+	printf("no. %d\n", i);
+	}
+
+	return (0);
+}
+
