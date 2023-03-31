a9db90379523868d03bf3b444b4ff110b7a7380b
grufarik <austinegomilo7@gmail.com>
2023-03-31 12:02:55 -0700

    branch_if_else

---
+++ Branching.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * checking if 5 is greatwer than 10
+ * If it is greater, "True" will be printed
+ * If it is smaller, "False" will be printed
+ * @i: the variable to be used
+ *
+ * Return: Always 0 when successful
+ */
+
+int main()
+{
+	int i=5;
+
+	if (5>10)
+		printf("True\n");
+	else
+		printf("False\n");
+	return (0);
+}
