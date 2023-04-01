13721ae8d76a30d794f83a3e4ca6a97e1c0d54e8
grufarik <austinegomilo7@gmail.com>
2023-04-01 08:26:54 -0700

    while

---
+++ while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Whille: the loop used to print "Terrific" seven times
+ * followed by a new line
+ * @i: the variable used.
+ *
+ * Return: Always 0 when successful
+ */
+
+int main ()
+{
+	int i=1;
+	
+	while (i<=7)
+	{
+		printf("Terrific\n");
+		i++;
+	}
+
+	return (0);
+}
